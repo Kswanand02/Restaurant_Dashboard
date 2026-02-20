d0819bac47dd0d57d6cedfb5ace5a5377408d0d7
Swanand Kamane <swanandkamane242@gmail.com>
2026-02-20 17:08:58 +0530

    Day 1: React app setup and Basic Layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return <>
+        <div>Navbar</div>
+        <ol>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Contact</li>
+        </ol>
+
+    </>
+
+
+}
+
+export default Navbar;
